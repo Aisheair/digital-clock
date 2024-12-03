10445899af9ca9741b81225ab62c2d60b8bc6b7c
Aisheair <ankitkargupta98313@gmail.com>
2024-12-03 20:16:26 +0530

    new

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 function updateClock(){
 
-    const now =new Date();
+    const now = new Date();
     const hours = now.getHours().toString().padStart(2, 0);
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const second = now.getSeconds().toString().padStart(2, 0);
